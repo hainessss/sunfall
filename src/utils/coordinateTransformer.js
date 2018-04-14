6a73b54da5f8c4fcaa03d22f8d88b476068f4461
Charles Buhler <charlesbuhler@gmail.com>
2018-04-14 09:11:08 -0700

    Adding initial CoordinateTransformer.transformCoordinateList.

---
+++ src/utils/coordinateTransformer.js
@@ -0,0 +1,11 @@
+/*
+ * Transforms lat/lng array to coordiante array
+ *
+ * 1. Stringifys lat/lngs
+ * 2. Adds Address, County, State
+ */
+const transformCoordinateList = coordinateList => {
+  console.log('passedCoordinate', coordinateList);
+}
+
+module.exports.transformCoordinateList = transformCoordinateList;
