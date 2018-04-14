7b4fae0e832c776168984ed9abe515b380747b28
Charles Buhler <charlesbuhler@gmail.com>
2018-04-14 09:31:31 -0700

    Transforming a single coordinate on click of map.

--- src/components/MapContainer/index.js
+++ src/components/MapContainer/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import styles from './index.css';
 import PropTypes from 'prop-types';
 import Map from '../Map';
+const { transformCoordinateList } = require('../../utils/coordinateTransformer');
 
 /**
  * Enter description.
- * 
+ *
  * @name MapContainer
  */
 class MapContainer extends React.Component {
@@ -27,13 +28,24 @@ class MapContainer extends React.Component {
     }
 
     onPlacesChanged = () => {
-        
+
     }
 
 
     onClick = (e) => {
         const { markers } = this.state;
         console.log('map click', e);
+
+        const lat = e.latLng.lat();
+        const lng = e.latLng.lng();
+        const coordinate = {
+          lat,
+          lng,
+        }
+
+        // Transforming a single coordinate
+        // TODO: Add to a list and transform only when full list is sent.
+        transformCoordinateList([coordiante]);
 
         this.setState({
             markers: markers.concat([
@@ -49,7 +61,7 @@ class MapContainer extends React.Component {
         const {markers} = this.state;
 
         return (
-                <Map 
+                <Map
                     googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
                     loadingElement={<div style={{ height: `100%` }} />}
                     containerElement={<div style={{ height: `600px` }} />}
--- src/utils/coordinateTransformer.js
+++ src/utils/coordinateTransformer.js
@@ -1,3 +1,22 @@
+const GOOGLE_PLACES = {
+  URL: 'https://maps.googleapis.com',
+  GEOCODE_URI: 'maps/api/geocode/json',
+  LAT_LNG: 'latlng',
+  KEY: 'key',
+}
+
+const GOOGLE_PLACES_SECRET_KEY = 'YOUR_API_KEY'
+
+/*
+ * Makes a reuqest to Google Places API and returns a Google Places Object
+ *
+ */
+const makeGeocodeRequest = (lat, lng) => {
+  const requestURL = `${GOOGLE_PLACES.URL}/${GOOGLE_PLACES.GEOCODE_URI}?${GOOGLE_PLACES.LAT_LNG}=${lat},${lng}`;
+
+  console.log('requestURL', requestURL);
+};
+
 /*
  * Transforms lat/lng array to coordiante array
  *
@@ -6,6 +25,12 @@
  */
 const transformCoordinateList = coordinateList => {
   console.log('passedCoordinate', coordinateList);
-}
+
+  for (const coordinate of coordinateList) {
+
+    makeGeocodeRequest(coordinate.lat, coordinate.lng);
+  }
+
+};
 
 module.exports.transformCoordinateList = transformCoordinateList;
